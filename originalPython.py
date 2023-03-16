9285a854f0b674945df6e836e5a579837281ef6d
Artur souza garcia <artursouzagarcia@gmail.com>
2023-03-15 23:44:58 -0300

    faltou algo :)

--- originalPython.py
+++ originalPython.py
@@ -69,3 +69,34 @@ def order_document_sections_by_query_similarity(query: str, contexts: Dict[Tuple
     return document_similarities
 
 order_document_sections_by_query_similarity("Who won the men's high jump?", document_embeddings)[:5]
+
+
+
+def construct_prompt(question: str, context_embeddings: dict, df: pd.DataFrame) -> str:
+    """
+    Fetch relevant 
+    """
+    most_relevant_document_sections = order_document_sections_by_query_similarity(question, context_embeddings)
+    
+    chosen_sections = []
+    chosen_sections_len = 0
+    chosen_sections_indexes = []
+     
+    for _, section_index in most_relevant_document_sections:
+        # Add contexts until we run out of space.        
+        document_section = df.loc[section_index]
+        
+        chosen_sections_len += document_section.tokens + separator_len
+        if chosen_sections_len > MAX_SECTION_LEN:
+            break
+            
+        chosen_sections.append(SEPARATOR + document_section.content.replace("\n", " "))
+        chosen_sections_indexes.append(str(section_index))
+            
+    # Useful diagnostic information
+    print(f"Selected {len(chosen_sections)} document sections:")
+    print("\n".join(chosen_sections_indexes))
+    
+    header = """Answer the question as truthfully as possible using the provided context, and if the answer is not contained within the text below, say "I don't know."\n\nContext:\n"""
+    
+    return header + "".join(chosen_sections) + "\n\n Q: " + question + "\n A:"
